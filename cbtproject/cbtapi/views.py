ed5b29ed8d7154eac7f8e9236adf035f974dc998
Olubusayo Oyekan <bussyice26@gmail.com>
2020-09-25 08:56:50 -0700

    deployed

--- cbtproject/cbtapi/views.py
+++ cbtproject/cbtapi/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-
+from django.http import HttpResponse
 from .serializers import ObjectiveSerializer
 from .serializers import TheorySerializer
 from .models import Objective
 from .models import Theory
 
 # Create your views here.
+
 class ObjectiveViewSet(viewsets.ModelViewSet):
     queryset = Objective.objects.all().order_by('question')
     serializer_class = ObjectiveSerializer
-
 
 class TheoryViewSet(viewsets.ModelViewSet):
     queryset = Theory.objects.all().order_by('question')
